9c9183ee6f0ea1873612eacbd468adeb6d8d9a11
Badr Chigar <132501656+badr-chigar@users.noreply.github.com>
2026-06-01 04:15:58 +0100

    Add files via upload

---
+++ backend/src/main/java/ma/legourmet/LegourmetApplication.java
@@ -0,0 +1,7 @@
+package ma.legourmet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class LegourmetApplication {
+    public static void main(String[] args){ SpringApplication.run(LegourmetApplication.class, args); }
+}
